d23a640ee40d485663da965f753f08228c33d0c8
raullopezepam <raul_lopez1@epam.com>
2026-03-04 11:20:54 +0100

    docs: deployment de kubernetes

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const http = require('http');
+
+// Aquí le decimos a Node: "Busca una variable llamada SALUDO_USER"
+// Si no la encuentras, usa el texto por defecto.
+const saludo = process.env.SALUDO_USER || "Hola (sin variable aún)";
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hola desde el frontend en Kubernetes!\n');
+  res.end(`${saludo}\n`);
 });
+
 server.listen(3000, () => {
   console.log('Servidor corriendo en el puerto 3000');
 });
